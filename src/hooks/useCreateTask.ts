9a8424ca6b452a7a682bd829931f0a89c867150c
luislanga <luis.langajr96@gmail.com>
2025-03-07 13:33:31 -0300

    fixed optimistic update on create

--- src/hooks/useCreateTask.ts
+++ src/hooks/useCreateTask.ts
@@ -3,28 +3,29 @@ import { useApi } from "../api";
 import { queryClient } from "../main";
 
 const createTask = async ({ title, description, status }: any, api: any) => {
-    const createTaskRequest = {
-      title,
-      description,
-      status
-    };
-    const response = await api.post("/task", createTaskRequest);
-    return response.data;
+  const createTaskRequest = {
+    title,
+    description,
+    status,
   };
+  const response = await api.post("/task", createTaskRequest);
+  return response.data;
+};
 
-  export const useCreateTask = () => {
-    const api = useApi();
-    return useMutation({
-      mutationFn: (variables: any) => createTask(variables, api),
-      onSuccess(data, variables) {
-        queryClient.setQueryData(["tasks"], (oldTasks: any[]) => [
-          ...oldTasks,
-          {
-            title: variables.title,
-            description: variables.description,
-            status: variables.status,
-          },
-        ]);
-      },
-    });
-  };
+export const useCreateTask = () => {
+  const api = useApi();
+  return useMutation({
+    mutationFn: (variables: any) => createTask(variables, api),
+    onSuccess(data, variables) {
+      queryClient.setQueryData(["tasks"], (oldTasks: any[]) => [
+        ...oldTasks,
+        {
+          id: data.data.id,
+          title: variables.title,
+          description: variables.description,
+          status: variables.status,
+        },
+      ]);
+    },
+  });
+};
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,9 +3,10 @@ import { CreateTaskModal } from "../components/CreateTaskModal/CreateTaskModal";
 import { Header } from "../components/Header/Header";
 import { Table } from "../components/Table/Table";
 import { WidthContainer } from "../components/WidthContainer/WidthContainer";
+import { Button } from "../components/Button/Button";
 
 export const Dashboard = () => {
-  const [isCreateTaskModalOpen, setIsCreateTaskModalOpen] = useState(true);
+  const [isCreateTaskModalOpen, setIsCreateTaskModalOpen] = useState(false);
 
   const handleOpenCreateTaskModal = () => {
     setIsCreateTaskModalOpen(true);
@@ -19,6 +20,7 @@ export const Dashboard = () => {
       <Header />
       <WidthContainer>
         <h1>Tarefas</h1>
+        <Button onClick={handleOpenCreateTaskModal}>Criar Tarefa</Button>
         <Table />
         {isCreateTaskModalOpen && (
           <CreateTaskModal onClose={handleCloseCreateTaskModal} />
